8515ac4859d594347b179ed0e13d5cdc350b872e
circleolami <circleolami@gmail.com>
2023-09-20 09:32:15 +0900

    week2

--- week2/test2/test2.asm
+++ week2/test2/test2.asm
@@ -1,12 +1,11 @@
 test2    start   1000                                            
 first    lda     one                                             
+         jsub    double                                          
          sta     result                                          
-double   lda     result                                          
-         mul     two                                             
-         sta     result                                          
-         lda     result                                          
+double   mul     two                                             
          comp    sixteen                                         
          jlt     double                                          
+         rsub                                                    
 one      word    1                                               
 two      word    2                                               
 sixteen  word    16                                              
